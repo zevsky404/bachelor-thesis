0d907853f7393b98b964be2cf34299f2b0383ec9
zevsky404 <chrissiguetter2001@gmail.com>
2023-11-14 17:11:16 +0100

    finalise tree drawing, test with longer thread

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import * as d3 from "d3";
-import {tsv} from "d3";
 
-const treemap = d3.tree().size([500, 500]);
-let thread = d3.stratify()([
+/*const data = [
     {id: "t3_1u26af"},
-    {id: "t1_cedrk20", parentId: "t3_1u26af"},
-    {id: "t1_cedthyd", parentId: "t1_cedrk20"},
-    {id: "t1_cedtudt", parentId: "t1_cedthyd"}
-]);
+    {id: "t1_cedrk20", parent_id: "t3_1u26af"},
+    {id: "t1_cedthyd", parent_id: "t1_cedrk20"},
+    {id: "t1_cedtudt", parent_id: "t1_cedrk20"}
+];*/
 
-thread = treemap(thread);
+const data = [{"id": "t3_11z1qpg"}, {"id": "t1_jdah58q", "parent_id": "t3_11z1qpg"}, {"id": "t1_jdai9xf", "parent_id": "t1_jdah58q"}, {"id": "t1_jdatppq", "parent_id": "t1_jdatdlp"}, {"id": "t1_jdais0f", "parent_id": "t1_jdai9xf"}, {"id": "t1_jdatdlp", "parent_id": "t1_jdastor"}, {"id": "t1_jdak4vp", "parent_id": "t1_jdais0f"}, {"id": "t1_jdaoj9b", "parent_id": "t1_jdak4vp"}, {"id": "t1_jdari8s", "parent_id": "t1_jdaoj9b"}, {"id": "t1_jdastor", "parent_id": "t1_jdari8s"}]
+
+let diagonal = function diagonal(d) {
+    return "M" + d.source.x + "," + d.source.y
+        + " C" + (d.source.x + d.target.x) / 2 + "," + d.source.y
+        + " " + (d.source.x + d.target.x) / 2 + "," + d.target.y
+        + " " + d.target.x + "," + d.target.y;
+};
+
+const tree = d3.tree().size([300, 300]);
+let root = d3.stratify()
+    .id(d => d.id)
+    .parentId(d => d.parent_id)
+    (data);
+
+root = tree(root);
+
+const links = root.links();
+
+console.log(root)
+console.log(links)
 
 const svg = d3.create("svg")
     .attr("viewBox", [0, 0, 500, 500])
@@ -19,12 +37,24 @@ const g = svg.append("g")
 
 
 const node = g.selectAll(".node")
-    .data(thread.descendants())
+    .data(root.descendants())
     .enter().append("g")
-    .attr("class", d => "node" + (d.children ? " node--internal"
-        : " node--leaf"))
-    .attr("transform", d => "translate(" + d.y + "," +
-        d.x + ")");
+    .attr("class", d => "node" + (d.children ? " node-internal"
+        : " node-leaf"))
+    .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
+const edge = g.selectAll(".edge")
+    .data(links)
+    .enter().append("path")
+    .attr("class", "edge")
+    .style("stroke", "black")
+    .style("fill", "none")
+    .attr("d", diagonal);
+
+node.append("circle")
+    .attr("r", "2")
+    .style("stroke", "black")
+    .style("fill", "none");
 
 let main = document.getElementById('main-container');
 main.appendChild(svg.node())
